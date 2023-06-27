a71b11743ddbec9404109a8854e58a6ba2b11c7a
u-1 <shimaichi5973@gamil.com>
2023-06-27 20:13:38 +0900

    条件分岐追加

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello world!");
 
         for (int i = 0; i < 10; i++) {
+            if (i < 5) {
             System.out.println("Hello");
         }
     }
